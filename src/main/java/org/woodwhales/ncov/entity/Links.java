381c2277376f8a87702078b497c44eef1e20d836
woodwhales <woodwhales@163.com>
2020-10-02 10:07:16 +0800

    update

--- src/main/java/org/woodwhales/ncov/entity/Links.java
+++ src/main/java/org/woodwhales/ncov/entity/Links.java
@@ -1,16 +1,21 @@
 package org.woodwhales.ncov.entity;
 
-import java.util.Date;
-
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Links {
+	@TableId(type = IdType.AUTO, value = "${column.dbName}")
 	private Long id;
+	@TableField(value = "${column.dbName}")
 	private String code;
 	private String url;
 	private String linkName;
